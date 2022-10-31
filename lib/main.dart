fb87d99010e2d727f08c672e0cb4cae8e3328866
Bimbimzzze <kresno.bimantoro@gmail.comm>
2022-10-31 14:00:37 +0700

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Furiture Apps',
+      title: 'Flutter Furniture Apps ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
